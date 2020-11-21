9aed6356d52fb9188607b55d25e3815c23455a69
danielpenchev98 <danielpenchev@abv.bg>
2020-11-21 16:13:19 +0200

    Update TSP

--- AI-Cource-FMI/TSP.cpp
+++ AI-Cource-FMI/TSP.cpp
@@ -5,38 +5,36 @@
 #include <algorithm>
 
 std::mt19937_64 rng;
-
-std::pair<int, int>* verteces;
-double** distances;
-int** population;
-int populationSize;
-double* fitness;
-int cityNumbers;
+std::vector<std::pair<int, int>> cities;
+std::vector<std::vector<double>> distances;
+std::vector<int*> population;
+std::vector<double> fitness;
+int newGenerationSize;
 const int tournamentSize = 10;
-int generationSize = 25000;
-const double mutationProbability = 0.05;
+const double mutationProb = 0.08;
+const double insertionMutProb = 1.0;
+const double reverseMutProb = 0.4;
+const double generateRandomProb = 0.2;
 
-inline const std::pair<int*,int*> cyclicCrossover(int* parentA, int* parentB) {
-	int* positionsA = new int[cityNumbers+1];
-	int* firstChild = new int[cityNumbers];
-	int* secondChild = new int[cityNumbers];
+inline const std::pair<int*, int*> cyclicCrossover(const int* const parentA, const int* const parentB) {
+	int* positionsA = new int[cities.size() + 1];
+	int* firstChild = new int[cities.size()];
+	int* secondChild = new int[cities.size()];
 
-
-	
-	for (int i = 0; i < cityNumbers; i++) {
+	for (int i = 0; i < cities.size(); i++) {
 		firstChild[i] = -1;
 		secondChild[i] = -1;
 		positionsA[parentA[i]] = i;
 	}
 
 	int flag = true;
-	for (int i = 0; i < cityNumbers; i++) {
+	for (int i = 0; i < cities.size(); i++) {
 		if (firstChild[i] != -1) {
 			continue;
 		}
 
 		int curr = i;
-		while (firstChild[curr]==-1) {
+		while (firstChild[curr] == -1) {
 			if (flag) {
 				firstChild[curr] = parentA[curr];
 				secondChild[curr] = parentB[curr];
@@ -51,266 +49,356 @@ inline const std::pair<int*,int*> cyclicCrossover(int* parentA, int* parentB) {
 		flag = !flag;
 	}
 
-	
-
 	delete[] positionsA;
-
 	return std::pair<int*, int*>(firstChild, secondChild);
 }
 
 inline void insertMutation(int* const arr) {
-	std::uniform_real_distribution<> dis(0.0, 1.0);
-	double prob = dis(rng);
-	if (prob > mutationProbability) {
-		return;
-	}
-
-	int firstCheckpoint = rng() % cityNumbers;
-	int secondCheckpoint = rng() % cityNumbers;
+	std::uniform_int_distribution<int> checkpointDistr(0, cities.size() - 1);
+	int firstCheckpoint = checkpointDistr(rng);
+	int secondCheckpoint = checkpointDistr(rng);
 	int temp = secondCheckpoint;
 
 	if (firstCheckpoint == secondCheckpoint) {
 		return;
-	} else if (firstCheckpoint > secondCheckpoint) {
+	}
+	else if (firstCheckpoint > secondCheckpoint) {
 		secondCheckpoint = firstCheckpoint;
 		firstCheckpoint = temp;
 	}
 
 	temp = arr[secondCheckpoint];
-	for (int i = secondCheckpoint -1; i > firstCheckpoint; i--) {
-		arr[i+1] = arr[i];
+	for (int i = secondCheckpoint - 1; i > firstCheckpoint; i--) {
+		arr[i + 1] = arr[i];
 	}
 	arr[firstCheckpoint + 1] = temp;
 }
 
-inline int getTournamentWinner(const std::vector<int>& competitors) {
+inline void reverseSequenceMutation(int* const arr) {
+	std::uniform_int_distribution<int> checkpointDistr(0, cities.size() - 1);
+	int firstCheckpoint = checkpointDistr(rng);
+	int secondCheckpoint = checkpointDistr(rng);
+	int temp = secondCheckpoint;
+
+	if (firstCheckpoint == secondCheckpoint) {
+		return;
+	}
+	else if (firstCheckpoint > secondCheckpoint) {
+		secondCheckpoint = firstCheckpoint;
+		firstCheckpoint = temp;
+	}
+
+	int areaSize = secondCheckpoint - firstCheckpoint + 1;
+	int center = areaSize / 2;
+	for (int i = 0; i < center; i++) {
+		temp = arr[i + firstCheckpoint];
+		arr[i + firstCheckpoint] = arr[secondCheckpoint - i];
+		arr[secondCheckpoint - i] = temp;
+	}
+}
+
+inline void mutate(int* const arr) {
+	std::uniform_real_distribution<double> dis(0.0, 1.0);
+	double prob = dis(rng);
+	if (prob > mutationProb) {
+		return;
+	}
+
+	double picker = dis(rng);
+	if (picker <= generateRandomProb) {
+		std::shuffle(arr, arr + cities.size(), rng);
+	}
+	else if (picker <= reverseMutProb) {
+		reverseSequenceMutation(arr);
+	}
+	else {
+		insertMutation(arr);
+	}
+
+}
+
+inline int getTournamentWinner(const std::vector<int>& competitors, const std::vector<double>& allCompetitorfitness) {
 	int winnerIdx = competitors[0];
-	double bestFitness = fitness[0];
+	double bestFitness = allCompetitorfitness[winnerIdx];
 	for (int compIdx : competitors) {
-		if (fitness[compIdx] < bestFitness) {
-			bestFitness = fitness[compIdx];
+		if (allCompetitorfitness[compIdx] < bestFitness) {
+			bestFitness = allCompetitorfitness[compIdx];
 			winnerIdx = compIdx;
 		}
 	}
 	return winnerIdx;
 }
 
-//for the surviving mechanism use maybe Crowding
-
-inline std::vector<int> tournamentSelection() {
-	std::vector<int> winners(25000);
-	std::vector<int> tournamentCompetitors(tournamentSize);
-	for (int i = 0; i < 25000; i++) {
-		for (int j = 0; j < tournamentSize; j++) {
-			//should a competitor play in the same tournament twice
-			int competitor = rng() % populationSize;
-			tournamentCompetitors[j] = competitor;
-		}
-		winners[i] = getTournamentWinner(tournamentCompetitors);
-	}
-	return winners;
-}
-
 //using stochastic universal sampling
-/*inline std::vector<int> rouletteWheenSelection() {
+inline std::vector<int> rouletteWheenSelection() {
+	int winnerCount = 0.3 * population.size();
 	double totalFitness = 0;
-	for (int i = 0; i < populationSize; i++) {
+	for (int i = 0; i < population.size(); i++) {
 		totalFitness += fitness[i];
 	}
 
-	double* commulativeFitness = new double[populationSize];
+	double* commulativeFitness = new double[population.size()];
 
 	commulativeFitness[0] = fitness[0] / totalFitness;
-	for (int i = 1; i < populationSize; i++) {
+	for (int i = 1; i < population.size(); i++) {
 		commulativeFitness[i] = commulativeFitness[i - 1] + (fitness[i] / totalFitness);
 	}
 
-	std::cout << commulativeFitness[populationSize - 1] << std::endl;
-
 	int curr = 0;
-	std::uniform_real_distribution<> dis(0.0, 1.0/generationSize);
+	std::uniform_real_distribution<> dis(0.0, 1.0 / winnerCount);
 	double r = dis(rng);
 	int i = 0;
 
 	std::vector<int> winners;
-	while (curr < generationSize) {
+	while (curr < winnerCount) {
 		while (r <= commulativeFitness[i]) {
 			winners.push_back(i);
 			curr++;
-			r += 1.0 / generationSize;
+			r += 1.0 / winnerCount;
 		}
 		i++;
 	}
+
+	delete[] commulativeFitness;
 	return winners;
-}*/
+}
 
 inline double calculateFitness(const int* const individual) {
 	double fitness = 0;
-	for (int i = 1; i < cityNumbers+1; i++) {
+	for (int i = 1; i < cities.size(); i++) {
 		fitness += distances[individual[i - 1]][individual[i]];
 	}
 	return fitness;
 }
 
-//needs big population + no duplicates - Replace worst(GENTITOR)
-inline void updatePopulation(const std::vector<int*> newGeneration) {
-	int** mergedPopulation = new int*[populationSize + newGeneration.size()];
-
-
-	double worstFitness = 0;
-	int* bestIndividual = nullptr;
-	double bestFitness = DBL_MAX;
-	for (int i = 0; i < populationSize + newGeneration.size(); i++) {
-		if (i < populationSize) {
-			mergedPopulation[i] = population[i];
-		}
-		else 
-		{
-			mergedPopulation[i] = newGeneration[i - populationSize];
-		}
-
-		double currFitness = calculateFitness(mergedPopulation[i]);
-		if (bestFitness > currFitness) {
-			bestFitness = currFitness;
-			bestIndividual = mergedPopulation[i];
-		}
-
-		if (worstFitness < currFitness) {
-			worstFitness = currFitness;
-		}
+inline const std::vector<int> roundRobinTournament(const std::vector<int*>& gladiators, const std::vector<double>& gladiatorFitness, int tournamentsNumbers, int winnerNumbers) {
+	std::vector<std::pair<int, int>> results(gladiators.size());
+	for (int i = 0; i < gladiators.size(); i++) {
+		results[i].first = i;
+		results[i].second = 0;
 	}
 
-	std::sort(mergedPopulation, mergedPopulation + populationSize+newGeneration.size(), [](const int* const l,const int* const r) {
-		return calculateFitness(l) < calculateFitness(r); });
-
-	std::cout << "BestFitness is :" << bestFitness <<" ";
-	std::cout << "Worst Fitness is :" << worstFitness << " ";
-	double mean = 0;
-	for (int i = 0; i < populationSize; i++) {
-		population[i] = mergedPopulation[i];
-		fitness[i] = calculateFitness(population[i]);
-		mean += fitness[i] / populationSize;
+	std::uniform_int_distribution<int> distr(0, gladiators.size() - 1);
+	std::vector<int> tournamentCompetitors(tournamentSize);
+	
+	for (int i = 0; i < tournamentsNumbers; i++) {
+		for (int j = 0; j < tournamentSize; j++) {
+			int competitor = distr(rng);
+			tournamentCompetitors[j] = competitor;
+		}
+		int winner = getTournamentWinner(tournamentCompetitors, gladiatorFitness);
+		results[winner].second++;
 	}
 
-	std::cout << "Mean :" << mean << std::endl;
+	//sort by their wins in descending order and also if have same win counter by fitness
+	std::sort(results.begin(), results.end(), [&](const std::pair<int, int>& l, const std::pair<int, int>& r) {
+		if (l.second == r.second) {
+			return gladiatorFitness[l.first] < gladiatorFitness[r.first];
+		}
+		return l.second > r.second; });
 
-	for (int i = 0; i < newGeneration.size(); i++) {
-		delete[] mergedPopulation[i+populationSize];
+	std::vector<int> winners(winnerNumbers);
+	for (int i = 0; i < winnerNumbers; i++) {
+		winners[winnerNumbers - i - 1] = results[i].first;
 	}
-	delete[] mergedPopulation;
+
+	//sort by descending fitness
+	std::sort(winners.begin(), winners.end(), [&](const int l, const int r) {
+		return gladiatorFitness[l] > gladiatorFitness[r]; });
+
+	return winners;
 }
 
-inline std::vector<int*> getNewGeneration(const std::vector<int>& winners) {
+//needs big population + no duplicates - 10% elitism + 90% and newgeneration round robin
+inline void updatePopulation(const std::vector<int*>& newGeneration) {
+	int elitism = 0.1 * population.size();
+
+	int competitiorNumbers = population.size() - elitism + newGeneration.size();
+
+	std::vector<int*> competitors(competitiorNumbers);
+	std::vector<double> competitorFitness(competitiorNumbers);
+
+	for (int i = 0; i < competitiorNumbers; i++) {
+		if (i < population.size() - elitism) {
+			competitors[i] = population[i];
+			competitorFitness[i] = fitness[i];
+		}
+		else
+		{
+			competitors[i] = newGeneration[i - population.size() + elitism];
+			competitorFitness[i] = calculateFitness(competitors[i]);
+		}
+	}
+
+	const std::vector<int>& winners = roundRobinTournament(competitors, competitorFitness, population.size(), population.size() - elitism);
+
+	for (int i = 0; i < population.size() - elitism; i++) {
+		population[i] = competitors[winners[i]];
+		fitness[i] = competitorFitness[winners[i]];
+		competitors[winners[i]] = nullptr;
+	}
+
+	//for small population like 1000 and also its almost sorted
+	for (int i = population.size() - elitism; i < population.size(); i++) {
+		int* temp = population[i];
+		double fit = fitness[i];
+		int j = i - 1;
+		for (; j >= 0 && fitness[j] < fit; j--) {
+			population[j + 1] = population[j];
+			fitness[j + 1] = fitness[j];
+		}
+		fitness[j + 1] = fit;
+		population[j + 1] = temp;
+	}
+
+	for (int i = 0; i < population.size() - elitism + newGeneration.size(); i++) {
+		if (competitors[i] == nullptr) continue;
+		delete[] competitors[i];
+	}
+}
+
+inline void showPopulationStatistics() {
+	double mean = 0;
+	double bestFitness = DBL_MAX;
+	double worstFitness = 0;
+	for (int i = 0; i < population.size(); i++) {
+		mean += fitness[i] / population.size();
+		if (bestFitness > fitness[i]) {
+			bestFitness = fitness[i];
+		}
+
+		if (worstFitness < fitness[i]) {
+			worstFitness = fitness[i];
+		}
+	}
+
+	std::cout << "Best :" << bestFitness << " Worst :" << worstFitness << " Mean :" << mean << std::endl;
+}
+
+inline const std::vector<int*> getNewGeneration(const std::vector<int>& winners) {
 	std::vector<int*> newGeneration;
-	for (int i = 0; i < generationSize; i++) {
-		int firstParent = winners[rng() % winners.size()];
-		int secondParent = winners[rng() % winners.size()];
+	for (int i = 0; i < newGenerationSize; i++) {
+		std::uniform_int_distribution<int> distr(0, winners.size() - 1);
+		int firstParent = winners[distr(rng)];
+		int secondParent = winners[distr(rng)];
 
 		//crossover
 		std::pair<int*, int*> children = cyclicCrossover(population[firstParent], population[secondParent]);
 
 		//mutation
-		insertMutation(children.first);
-		insertMutation(children.second);
+		mutate(children.first);
+		mutate(children.second);
 
 		newGeneration.push_back(children.first);
 		newGeneration.push_back(children.second);
 	}
 
+	for (int i = 0; i < newGenerationSize; i++) {
+		if (newGeneration[i] == nullptr) {
+			exit(1);
+		}
+	}
+
 	return newGeneration;
 }
 
-int main() {
+void initCities() {
+	int cityNumbers = 0;
 	std::cin >> cityNumbers;
-
-	verteces = new std::pair<int, int>[cityNumbers];
-	rng.seed(std::random_device{}());
+	cities.resize(cityNumbers);
+	distances.resize(cities.size());
 
 	int x, y;
-	for (int i = 0; i < cityNumbers; i++) {
+	for (int i = 0; i < cities.size(); i++) {
 		std::cin >> x;
+		cities[i].first = x;
 		std::cin >> y;
-		verteces[i] = std::pair<int, int>(x, y);
+		cities[i].second = y;
+		distances[i].resize(cities.size());
 	}
 
-	
-	distances = new double* [cityNumbers];
-	for (int i = 0; i < cityNumbers; i++) {
-		distances[i] = new double[cityNumbers];
-		for (int j = 0; j < cityNumbers; j++) {
-			distances[i][j] = sqrt((verteces[i].first - verteces[j].first) * (verteces[i].first - verteces[j].first) +
-				(verteces[i].second - verteces[j].second) * (verteces[i].second - verteces[j].second));
+	for (int i = 0; i < cities.size(); i++) {
+		for (int j = 0; j < cities.size(); j++) {
+			distances[i][j] = sqrt((cities[i].first - cities[j].first) * (cities[i].first - cities[j].first) +
+				(cities[i].second - cities[j].second) * (cities[i].second - cities[j].second));
 		}
 	}
+}
 
+void initPopulation() {
+	int populationSize;
 	std::cout << "Choose population number :";
 	std::cin >> populationSize;
-	
-	int* arr = new int[cityNumbers];
-	for (int i = 0; i < cityNumbers; i++) {
+
+	//Set children to be 20% of the population
+	newGenerationSize = 0.3 * populationSize;
+
+	int* arr = new int[cities.size()];
+	for (int i = 0; i < cities.size(); i++) {
 		arr[i] = i;
 	}
 
-	/*arr[0] = 40;
-	arr[1] = 15;
-	arr[2] = 21;
-	arr[3] = 0;
-	arr[4] = 7;
-	arr[5] = 8;
-	arr[6] = 37;
-	arr[7] = 30; arr[8] = 43; arr[9] = 17; arr[10] = 6; arr[11] = 27;
-	arr[12] = 5; arr[13] = 36; arr[14] = 18; arr[15] = 26; arr[16] = 16; arr[17] = 42; arr[18] = 29; arr[19] = 35; arr[20] = 45; arr[21] = 32; arr[22] = 19;arr[23] = 10; arr[24] = 22;
-	arr[25] = 12; arr[26] = 24; arr[27] = 13; arr[28] = 33; arr[29] = 2; arr[30] = 39; arr[31] = 14; arr[32] = 11; arr[33] = 46; arr[34] = 20; arr[35] = 31; arr[36] = 38; arr[37] = 47; arr[38] = 4;
-	arr[39] = 28; arr[40] = 1; arr[41] = 3; arr[42] = 25; arr[43] = 41; arr[44] = 23; arr[45] = 9; arr[46] = 34; arr[47] = 44; arr[48] = 40;*/
-
-	std::shuffle(arr, arr + cityNumbers, rng);
-	std::shuffle(arr, arr + cityNumbers, rng);
-	std::shuffle(arr, arr + cityNumbers, rng);
-	std::shuffle(arr, arr + cityNumbers, rng);
-	population = new int* [populationSize];
-	fitness = new double[populationSize];
-	for (int i = 0; i < populationSize; i++) {
-		population[i] = new int[cityNumbers];
-		std::shuffle(arr, arr + cityNumbers, rng);
-		for (int j = 0; j < cityNumbers; j++) {
+	population.resize(populationSize);
+	fitness.resize(populationSize);
+	for (int i = 0; i < population.size(); i++) {
+		population[i] = new int[cities.size()];
+		std::shuffle(arr, arr + cities.size(), rng);
+		for (int j = 0; j < cities.size(); j++) {
 			population[i][j] = arr[j];
 		}
+	}
 
+	std::sort(population.begin(), population.end(), [](const int* const l, const int* const r) {
+		return calculateFitness(l) > calculateFitness(r); });
+
+	for (int i = 0; i < population.size(); i++) {
 		fitness[i] = 0;
-		for (int j = 1; j < cityNumbers; j++) {
+		for (int j = 1; j < cities.size(); j++) {
 			fitness[i] += distances[population[i][j - 1]][population[i][j]];
 		}
 	}
-	int thresholdGenerations = 5000;  
+}
+
+int main() {
+	rng.seed(std::random_device{}());
+	initCities();
+	initPopulation();
+
+	int thresholdGenerations = 400;
 	int currGeneration = 0;
-	while (currGeneration<=thresholdGenerations) {
-		std::cout << "Generation :" << currGeneration <<" ";
+	while (currGeneration <= thresholdGenerations) {
+		//std::cout << "Generation :" << currGeneration << " ";
 		//Selection step
-		const std::vector<int>& winners = tournamentSelection();
-		
+		const std::vector<int>& winners = rouletteWheenSelection();//tournamentSelection();
+
 		//Breeding step
 		const std::vector<int*>& newGeneration = getNewGeneration(winners);
 
 		//Survival step
 		updatePopulation(newGeneration);
+		//showPopulationStatistics();
 
 		currGeneration++;
 	}
-	
+
 	int bestIndx = 0;
 	double bestFitness = DBL_MAX;
-	for (int i = 0; i < populationSize; i++) {
+	for (int i = 0; i < population.size(); i++) {
 		if (fitness[i] < bestFitness) {
 			bestFitness = fitness[i];
 			bestIndx = i;
 		}
 	}
-	
-	std::cout << bestFitness<<" ";
 
-	for (int i = 0; i < cityNumbers; i++) {
-		std::cout << population[bestIndx][i]<<" ";
+	std::cout << bestFitness << " ";
+
+	for (int i = 0; i < cities.size(); i++) {
+		std::cout << population[bestIndx][i] << " ";
 	}
-	
+
+	for (int i = 0; i < population.size(); i++) {
+		delete[] population[i];
+	}
+
 	return 0;
 }
