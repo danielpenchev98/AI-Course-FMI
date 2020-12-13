2a492451c0ed1a4ebc0ad84ddfa853a8505c179e
i515142 <daniel.penchev@sap.com>
2020-12-13 23:43:18 +0200

    Fix compilation error

--- DecisionTree/ID3.jl
+++ DecisionTree/ID3.jl
@@ -10,7 +10,8 @@ using InvertedIndices
 mutable struct ID3
     feature::Int64
     children::Array{ID3,1}
-    class::String
+    dominatingClass::String
+    isLeaf::Bool
 end
 
 #we assume that the DataFrame arg is already filtered suck that it already uses all previously selected features
@@ -18,76 +19,106 @@ end
 
 #TODO use laplace smoothing
 
-function entropyOneAttribute(df::DataFrame,classDomain::Array{String,1})
+function entropyOneAttribute(df::DataFrame,classDomain::Array{String,1}; α=0.005)
     allCounts = nrow(df)
+    #propbabilities = map(class -> (count(df[:,1] .== class) + α) /(allCounts + α * length(classDomain[1])) ,classDomain)
     propbabilities = map(class -> count(df[:,1] .== class)/allCounts ,classDomain)
-    println(propbabilities)
-    propbabilities = ["sfa"]
-    return - foldr((prob, res) -> res + prob * log2(prob) ,propbabilities,init=0.0)
+    return - foldr((prob, res) -> prob == 0.0 ? res : res + prob * log2(prob) ,propbabilities,init=0.0)
 end
 
-function entropyTwoAttributes(df::DataFrame,feature::Int64,featureDomain::Array{String,1},classDomain::Array{String,1})
-    allCounts = nrow(df)
-
-    ∑ = 0
-
+function entropyTwoAttributes(df::DataFrame,feature::Int64,featureDomain::Array{String,1},classDomain::Array{String,1}; α = 0.005)
+    allCounts, ∑ = nrow(df), 0.0
     for featureValue in featureDomain
-        enrop = entropyOneAttribute( df[df[:,feature] .== featureValue,:],classDomain)
-        propOfFeature = length(df[:,feature] .== featureValue)/allCounts
-        @printf("Feature %d with value %s has single entropy %.6f abd propOfFeature %.6f\n",feature,featureValue,enrop,propOfFeature)
-        ∑ += enrop * propOfFeature
+        entropy = entropyOneAttribute( df[df[:,feature] .== featureValue,:],classDomain)
+        #propOfFeature = (count(df[:,feature] .== featureValue) + α) / (allCounts + α * length(unique(df[:,feature])))
+        propOfFeature = count(df[:,feature] .== featureValue)/ allCounts
+        @printf("Feature %d with value %s has entropy %.6f and prob %.6f\n",feature,featureValue,entropy,propOfFeature)
+        ∑ += entropy * propOfFeature
     end
-
     return ∑
-    #return sum([entropyOneAttribute( df[df[:,feature] .== featureValue,:],classDomain)
-    #           * length(df[:,feature] .== featureValue)/allCounts for featureValue in featureDomain])
 end
-function calcGain(currentEnropy,parentEntropy)
-    if isempty(parentEntropy)
-        return currentEnropy
+
+function calcGain(currentEntropy,parentEntropy)
+    return parentEntropy - currentEntropy
+end
+
+function getDominatingClass(df::DataFrame, classDomain::Array{String,1}; parentDominatingClass = "Sentinel" )
+    winners = []
+    dominatingCount = 0
+    for class in classDomain
+        cnt = count(df[:,1] .== class)
+        if dominatingCount < cnt
+            dominatingCount = cnt
+            winners = [class]
+        elseif dominatingCount == cnt
+            push!(winners,class)
+        end
     end
-    return parentEntropy - currentEnropy
+
+    if length(winners) == 1
+        @printf("Winner is %s\n",winners[1])
+        @printf("Winner is %s\n",winners[1])
+        return winners[1]
+    elseif parentDominatingClass != "Sentinel" && parentDominatingClass in winner
+        @printf("Winner is %s\n",parentDominatingClass)
+        @printf("Winner is %s\n",parentDominatingClass)
+        return parentDominatingClass
+    end
+
+
+    return winners[(rand(Int) % length(winners))+1]
 end
 
-# P(x|C,y) = P(x,C,y)/P(C,y)
-
-function createID3(df::DataFrame,domains::Array{Array{String,1},1};setThreshold=5)
-    maxGain = 0
-    winner = 2
+function getFeatureWithBestGain(df::DataFrame, domains::Array{Array{String,1},1},currEntropy::Float64)
+    winner, maxGain = 2, -Inf
+    println(size(domains,1))
+    println(domains)
     for i in 2:size(domains,1)
-        gain = calcGain(entropyTwoAttributes(df,i,domains[i],domains[1]),[])
+        gain = calcGain(entropyTwoAttributes(df,i,domains[i],domains[1]),currEntropy)
         if maxGain < gain
             maxGain = gain
             winner = i
         end
     end
+    return winner
+end
 
-    root = ID3(winner,ID3[],"None")
+function createID3(df::DataFrame,domains::Array{Array{String,1},1};setThreshold=5)
+    println("------------")
+    println(domains)
+    println(df)
+    dominatingClass = getDominatingClass(df,domains[1])
+    currEntropy = entropyOneAttribute(df,domains[1])
+
+    winner = getFeatureWithBestGain(df,domains,currEntropy)
+    root = ID3(winner,ID3[],dominatingClass,false)
+
     for featureVal in domains[winner]
-        push!(root.children,createID3Recursive(df[df[:,winner] .== featureVal],domains[Not(winner)],setThreshold))
+        filteredSet = df[df[:,winner] .== featureVal,:]
+        child = createID3Recursive(filteredSet,domains[Not(winner)],setThreshold,dominatingClass)
+        push!(root.children, child)
     end
     return root
 end
 
-function creatID3Recursive(df::DataFrame,domains::Array{Array{String,1},1},setThreshold)
-    if nrow(df) >= setThreshold || length(domains) == 0
+function isHomogenous(currEntopy::Float64; ϵ=0.03)
+    return abs(currEntopy - 1.0) <= ϵ || currEntopy <= ϵ
+end
 
-        return ID3(-1,ID3[],"")
+function createID3Recursive(df::DataFrame,domains::Array{Array{String,1},1},setThreshold, parentDominatingClass::String)
+    dominatingClass = getDominatingClass(df,domains[1],parentDominatingClass = parentDominatingClass)
+    currEntropy =  entropyOneAttribute(df,domains[1])
+    if nrow(df) <= setThreshold || length(domains) == 0 || isHomogenous(currEntropy)
+        return ID3(-1,ID3[],dominatingClass,true)
     end
 
-    maxGain = 0
-    winner = 2
-    for i in 2:size(domains,1)
-        gain = calcGain(entropyTwoAttributes(df,i,domains[i],domains[1]),[])
-        if maxGain < gain
-            maxGain = gain
-            winner = i
-        end
-    end
-
+    winner = getFeatureWithBestGain(df,domains,currEntropy)
     root = ID3(winner,ID3[])
+
     for featureVal in domains[winner]
-        push!(root.children,createID3Recursive(df[df[:,winner] .== featureVal],domains[Not(winner)],setThreshold))
+        filteredSet = df[df[:,winner] .== featureVal]
+        child = createID3Recursive(filteredSet,domains[Not(winner)],setThreshold,dominatingClass)
+        push!(root.children, child)
     end
     return root
 end
@@ -145,10 +176,10 @@ domains = [["Yes","No"],
            ["Rainy","Overcast","Sunny"],
            ["Hot","Mild","Cool"],
            ["High","Normal"],
-           [""],
            ["True","False"]]
 
 permutecols!(df, [:Column5, :Column1,:Column2,:Column3,:Column4])
 
 
-entropyTwoAttributes(df,2,domains[2],domains[1])
+#entropyTwoAttributes(df,2,domains[2],domains[1])
+createID3(df,domains)
