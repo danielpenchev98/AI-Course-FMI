67e2988e8dde63304318a7a33324b7b45aa8818d
ci-runtime-serviceuser <eiriniintegrationteam@groups.sap.com>
2020-12-23 16:35:56 +0200

    Add confusion matrix metric

--- DecisionTree/ID3.jl
+++ DecisionTree/ID3.jl
@@ -7,14 +7,22 @@ using Distributions
 using Random
 using InvertedIndices
 
-struct ID3Node
-    feature::Int64
-    children::Array{ID3Node,1}
-    dominatingClass::String
-    isLeaf::Bool
-end
+#TODO at confusion matrix for accuracy evaluation
 
-#TODO could only pass only the column with the classes
+struct ID3Node
+    feature::Int64 #from 1 to the number of columns, none determined if -1
+    children::Array{ID3Node,1} #sub decision trees
+    dominatingClass::String #the dominating class currently
+    isLeaf::Bool #if terminal node
+ end
+
+ mutable struct ConfusionMatrix
+     truePositive::Int64
+     falsePositive::Int64
+     trueNegative::Int64
+     falseNegative::Int64
+ end
+
 function entropyOneAttribute(df::DataFrame,classDomain::Array{String,1}; α=0.005)
     allCounts = nrow(df)
     propbabilities = map(class -> (count(df[:,1] .== class) + α) /(allCounts + α * length(classDomain[1])) ,classDomain)
@@ -32,8 +40,7 @@ function entropyTwoAttributes(df::DataFrame,feature::Int64,featureDomain::Array{
 end
 
 function getDominatingClass(df::DataFrame, classDomain::Array{String,1}, parentDominatingClass)
-    winners = []
-    dominatingCount = 0
+    winners, dominatingCount = [], 0
     for class in classDomain
         cnt = count(df[:,1] .== class)
         if dominatingCount < cnt
@@ -47,9 +54,10 @@ function getDominatingClass(df::DataFrame, classDomain::Array{String,1}, parentD
         return winners[1]
     elseif parentDominatingClass != "Sentinel" && parentDominatingClass in winners
         return parentDominatingClass
+    else
+        return winners[(rand(UInt64) % length(winners))+1]
     end
 
-    return winners[(rand(Int) % length(winners))+1]
 end
 
 function calcGain(df::DataFrame,featureIdx::Int64,domains::Array{Array{String,1},1},parentEntropy::Float64)
@@ -57,10 +65,11 @@ function calcGain(df::DataFrame,featureIdx::Int64,domains::Array{Array{String,1}
     return parentEntropy - childEntropy
 end
 
-function getFeatureWithBestGain(df::DataFrame, domains::Array{Array{String,1},1},remainingFeatures::Array{Int64,1})
+
+function getFeatureWithBestGain(df::DataFrame, domains::Array{Array{String,1},1},competingFeatures::Array{Int64,1})
     winner, maxGain = -Inf, -Inf
     currEntropy = entropyOneAttribute(df,domains[1])
-    for i in remainingFeatures
+    for i in competingFeatures
         currGain = calcGain(df,i,domains,currEntropy)
         if maxGain < currGain
             winner, maxGain = i, currGain
@@ -69,28 +78,27 @@ function getFeatureWithBestGain(df::DataFrame, domains::Array{Array{String,1},1}
     return winner
 end
 
-function createID3Node(df::DataFrame,domains::Array{Array{String,1},1};setThreshold=14)
+
+function createID3Tree(df::DataFrame,domains::Array{Array{String,1},1};setThreshold=14)
     if nrow(df) == 0
         throw(ArgumentError("The dataset is empty"))
     elseif size(df,2) != size(domains,1)
         throw(ArgumentError("Number of features doesnt match the number of the domains"))
     end
 
-    remainingFeatures = [i for i in 2:size(df,2)]
-    return createID3NodeRecursive(df,domains,remainingFeatures,setThreshold,"Sentinel")
+    remainingFeatures = [i for i in 2:size(df,2)] #the first column is the class column
+    return createID3TreeRecursive(df,domains,remainingFeatures,setThreshold,"Sentinel")
 end
 
 function isHomogenous(df::DataFrame)
     return length(unique(df[:,1])) == 1
 end
 
-function createID3NodeRecursive(df::DataFrame,domains::Array{Array{String,1},1}, remainingFeatures::Array{Int64,1}, setThreshold::Int64, parentDominatingClass::String)
+function createID3TreeRecursive(df::DataFrame,domains::Array{Array{String,1},1}, remainingFeatures::Array{Int64,1}, setThreshold::Int64, parentDominatingClass::String)
     if nrow(df) == 0
         return ID3Node(-1,ID3Node[],parentDominatingClass,true)
     end
-
     dominatingClass = getDominatingClass(df,domains[1],parentDominatingClass)
-
     if nrow(df) <= setThreshold || length(remainingFeatures) == 0 || isHomogenous(df)
         return ID3Node(-1,ID3Node[],dominatingClass,true)
     end
@@ -101,23 +109,23 @@ function createID3NodeRecursive(df::DataFrame,domains::Array{Array{String,1},1},
     children = ID3Node[]
     for featureVal in domains[winner]
         filteredSet = df[df[:,winner] .== featureVal,:]
-        child = createID3NodeRecursive(filteredSet,domains,remainingFeatures,setThreshold,dominatingClass)
+        child = createID3TreeRecursive(filteredSet,domains,remainingFeatures,setThreshold,dominatingClass)
         push!(children, child)
     end
     return ID3Node(winner,children,dominatingClass,false)
 end
+
 
 function pickValue(valueProbabilities::Array{Float64,1}, domain::Any)
     result = rand(Multinomial(1, valueProbabilities))
     return domain[findfirst(x -> x == 1, result)]
 end
 
-#TODO think of a true reason why the some of the demain values of this feature have 0% chance - maybe laplace smoothing + normalization
 function fillValues!(df::DataFrame, domains::Array{Array{String,1},1})
     for col in 1:ncol(df)
         cnts = map(val -> count(df[:,col] .== val), domains[col])
         distribution = 1.0 * cnts ./ sum(cnts)
-        missingValueRows = foldr((row, res) -> ismissing(df[row,col]) ? push!(res,row) : res, 1:nrow(df), init=[])
+        missingValueRows = foldr((row, res) -> df[row,col]=="?" ? push!(res,row) : res, 1:nrow(df), init=[])
         for row in missingValueRows
             df[row,col] = pickValue(distribution,domains[col])
         end
@@ -139,13 +147,34 @@ end
 
 function validateModel(validateSet::DataFrame,tree::ID3Node, domains::Array{Array{String,1},1})
     success::Int64 = 0
-    for i = 1:size(validateSet, 1)
-        classIdx = classify(validateSet[i, Not(1)],tree,domains)
-        if classIdx == validateSet[i, 1]
+    for test in eachrow(validateSet)
+        classIdx = classify(test[Not(1)],tree,domains) #skip first column
+        if classIdx == test[1]
             success += 1
         end
     end
     return (1.0 * success) / size(validateSet, 1)
+end
+
+#Only for binary classification
+function validateModelConfusionMatrix(validateSet::DataFrame,tree::ID3Node, domains::Array{Array{String,1},1})
+    confMatrix = ConfusionMatrix(0.0,0.0,0.0,0.0)
+    for test in eachrow(validateSet)
+        classIdx = classify(test[Not(1)],tree,domains) #skip first column
+        if classIdx == 2
+            if classIdx == test[1]
+                confMatrix.truePositive += 1
+            else
+                confMatrix.falsePositive += 1
+            end
+        elseif classIdx == test[1]
+            confMatrix.trueNegative += 1
+        else
+            confMatrix.falseNegative += 1
+        end
+
+    end
+    return confMatrix
 end
 
 function stratifiedKFold(df::DataFrame,classDomain::Array{String,1}, folds::Int64)
@@ -156,33 +185,75 @@ function stratifiedKFold(df::DataFrame,classDomain::Array{String,1}, folds::Int6
     plan = []
     foldSize = convert(Int64, floor(size(df,1)/folds))
     for i in 1:folds
+        fold = []
         for j in 1:size(classDomain,1)
-            chunkSize = convert(Int64 ,floor(distributions[j] * foldSize +  ((i+j)%2==0 ? 1 : 0)))
-            append!(plan,indecesPerClass[j][1:chunkSize])
+            chunkSize = min(length(indecesPerClass[j]),convert(Int64 ,ceil(distributions[j] * foldSize)))
+            append!(fold,indecesPerClass[j][1:chunkSize])
             deleteat!(indecesPerClass[j],1:chunkSize)
         end
+        push!(plan,fold)
     end
 
-    return plan, foldSize
+    return plan
 end
 
-function DecisionTreeClassification(df::DataFrame, domains::Array{Array{String,1},1}; kfold = 10, setMinPopulation = 14)
-    kFoldPlan::Array{Int64,1}, sampleSize::Int64 = stratifiedKFold(df,domains[1],kfold)
+function DecisionTreeClassification(df::DataFrame, domains::Array{Array{String,1},1}; kfold = 20, setMinPopulation = 14)
+    kFoldPlan::Array{Array{Int64,1},1} = stratifiedKFold(df,domains[1],kfold)
     meanAccuracy::Float64 = 0
 
     for i in 1:kfold
-        trainDataSetIndeces = kFoldPlan[Not((i-1)*sampleSize+1:i*sampleSize)]
-        tree = createID3Node(df[trainDataSetIndeces,:],domains, setThreshold = setMinPopulation)
+        trainDataSetIndeces = reduce(vcat,kFoldPlan[Not(i)]) #merge all training folds into one
+        tree = createID3Tree(df[trainDataSetIndeces,:],domains, setThreshold = setMinPopulation)
 
-        accuracy::Float64 = validateModel( df[Not(trainDataSetIndeces),:],tree,domains)
+        accuracy::Float = validateModel(df[kFoldPlan[i],:],tree,domains)
+
         meanAccuracy += accuracy / kfold
     end
 
     return meanAccuracy
 end
 
-#=
-df = CSV.read("./data/breast-cancer.data",header=false,types=[String for i in 1:10])
+#Binary classification
+function DecisionTreeClassificationConfMatrix(df::DataFrame, domains::Array{Array{String,1},1}; kfold = 10, setMinPopulation = 14)
+    kFoldPlan::Array{Array{Int64,1},1} = stratifiedKFold(df,domains[1],kfold)
+    accumulativeConfMatrix = ConfusionMatrix(0.0,0.0,0.0,0.0)
+
+    for i in 1:kfold
+        trainDataSetIndeces = reduce(vcat,kFoldPlan[Not(i)]) #merge all training folds into one
+        tree = createID3Tree(df[trainDataSetIndeces,:],domains, setThreshold = setMinPopulation)
+
+        confMatrix = validateModelConfusionMatrix(df[kFoldPlan[i],:],tree,domains)
+
+        accumulativeConfMatrix.truePositive += confMatrix.truePositive
+        accumulativeConfMatrix.falsePositive += confMatrix.falsePositive
+        accumulativeConfMatrix.trueNegative += confMatrix.trueNegative
+        accumulativeConfMatrix.falseNegative += confMatrix.falseNegative
+    end
+
+    return accumulativeConfMatrix
+end
+
+function checkForFeatures(remainingFeatures::Array{Int64,1})
+    featuresSets = []
+    for i in 2:length(remainingFeatures)
+        set = [i]
+        for j in i:length(remainingFeatures)
+            if j != i
+                push!(set,j)
+            end
+
+            push!(featuresSets,copy(set))
+
+            if j != i
+                pop!(set)
+            end
+        end
+    end
+    return featuresSets
+end
+
+df = DataFrame(CSV.read("./data/breast-cancer.data",header=false,types=[String for i in 1:10]))
+df = unique(df) #remove duplicates
 
 const missingValuesThreshold = 1.0 * size(df, 2) / 2
 const missingValueSign = "?"
@@ -190,17 +261,34 @@ const missingValueSign = "?"
 domains = [unique(filter(x->x!=missingValueSign,df[:,i])) for i in 1:ncol(df)]
 fillValues!(df,domains)
 
-bestPopulationThreshold = 1
-bestMeanAccuracy = 0.0
 
-for i in 1:20
-    df = df[shuffle(1:size(df, 1)), :];
-    meanAcc = DecisionTreeClassification(df,domains,setMinPopulation = i)
-    if bestMeanAccuracy < meanAcc
-        bestMeanAccuracy = meanAcc
-        bestPopulationThreshold = i
+
+#=
+for mssingFeatures in checkForFeatures([i for i in 1:size(df,2)])
+    newDomains=domains[Not(mssingFeatures)]
+
+    bestPopulationThreshold = 1
+    bestMeanAccuracy = 0.0
+    for i in 1:20
+        meanAcc = 0.0
+        meanAccGivenSetThreshold = 0.0
+        for j in 1:10
+            df = df[shuffle(1:size(df, 1)), :];
+            meanAcc += DecisionTreeClassification(df[:,Not(mssingFeatures)],newDomains,setMinPopulation = i) / 10
+        end
+        if bestMeanAccuracy < meanAcc
+            bestMeanAccuracy = meanAcc
+            bestPopulationThreshold = i
+        end
+        #@printf("For setThreshold %d the accuracy is %.6f\n",i,meanAcc)
     end
-end
+    println("Missing features :")
+    println(mssingFeatures)
+    @printf("Mean accuracy %.6f with population threshold %d \n",bestMeanAccuracy,bestPopulationThreshold)
 
-@printf("Mean accuracy %.6f with population threshold %d\n",bestMeanAccuracy,bestPopulationThreshold)
-=#
+end=#
+
+
+df = df[shuffle(1:size(df, 1)), :];
+confMatrix = DecisionTreeClassificationConfMatrix(df[:,Not([2,5])], domains[Not(2,5)],setMinPopulation = 12)
+println(confMatrix)
