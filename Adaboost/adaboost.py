55bda4c43ef1658726650f55954dac458651895a
danielpenchev98 <danielpenchev@abv.bg>
2021-08-17 23:26:38 +0300

    Refactor

--- Adaboost/adaboost.py
+++ Adaboost/adaboost.py
@@ -1,99 +1,113 @@
 #!/usr/bin/env python3
 from os import truncate
 
+from math import log
+from typing import Dict
 import numpy as np
 import pandas as pd
-from typing import Dict
-from math import log
+
+ENTITY_WEIGHT_COLUMN = 'Sample Weight'
+CLASS_COLUMN = 'Class'
 
 class TreeStump:
-    def __init__(self, weight: np.ndarray, featureName: str, classification: Dict):
+    def __init__(self, weight: np.ndarray, feature_name: str, classification: Dict):
         self.weight = weight
-        self.featureName = featureName
+        self.featureName = feature_name
         self.classification = classification
 
     def train(self,dataset:pd.DataFrame)->None:
-        classColumnName = dataset.columns[-2]
         featureValues = dataset[self.featureName].unique()
         for featureVal in featureValues:
-            self.classification[featureVal] = dataset.loc[dataset.loc[:,self.featureName] == featureVal,classColumnName].value_counts().idxmax()
+            self.classification[featureVal] = dataset.loc[dataset.loc[:,self.featureName] == featureVal,CLASS_COLUMN].value_counts().idxmax()
+
+    def classify(self, entity: pd.Series):
+        return self.classification(entity[self.featureName])
 
 class AdaboostClassifier:
     def __init__(self):
         self.stumps = []
-        self.stumpWeightsColumn = 'Sample Weight'
 
     def train(self, dataset: pd.DataFrame)->None:
-        dataset[self.stumpWeightsColumn] =  np.ones(len(dataset))/len(dataset)
+        dataset[ENTITY_WEIGHT_COLUMN] =  np.ones(len(dataset))/len(dataset)
 
+        default_stump_weight = 1/2
         while True:
             feature = pick_next_feature(dataset)
-            stump = TreeStump(1/2,feature,{})
+            stump = TreeStump(default_stump_weight,feature,{})
             stump.train(dataset)
 
-            missclassifiedExamples = get_missclassified(dataset,stump)
-            if len(missclassifiedExamples) == 0:
+            misclassified_indices = get_missclassified(dataset,stump)
+            if len(misclassified_indices) == 0:
                 break
 
-            error = np.sum(dataset.loc[missclassifiedExamples,self.stumpWeightsColumn])
-            stump.weight = 1/2 * log((1-error)/error)
+            error = self._calculate_error(dataset, misclassified_indices)
+            stump.weight = self._calculate_stump_weight(error)
 
-            update_sample_weights(dataset, missclassifiedExamples,error, self.stumpWeightsColumn)
-            futureSampleIndices = np.random.choice(np.array([i for i in range(len(dataset))]),len(dataset),p=dataset.loc[:,self.stumpWeightsColumn])
-        
-
-            dataset = dataset.loc[futureSampleIndices,:]
-            dataset.reset_index(drop=True, inplace=True)
-        
+            update_sample_weights(dataset, misclassified_indices,error)
             self.stumps.append(stump)
 
+            dataset = sample_dataset(dataset)
+    
+    def _calculate_stump_weight(self,error: float) -> float:
+        return  1/2 * log((1-error)/error)
+
+    def _calculate_error(self,dataset: pd.DataFrame, misclassified_indices: np.ndarray)->int:
+        return np.sum(dataset.loc[misclassified_indices,ENTITY_WEIGHT_COLUMN])
+
+    def classify(self,entity: pd.Series):
+        return sum(stump.weight * stump.classify(entity) for stump in self.stumps)        
 
 def calculate_gini(dataset: pd.DataFrame, featureName: str)->float:
-    classFeatureName = dataset.columns[-2]
-    featureValues = np.sort(dataset.loc[:,featureName].unique())
-    sizePerValue = dataset.groupby(featureName).size()
+    feature_values = np.sort(dataset.loc[:,featureName].unique())
+    feature_value_count = dataset.groupby(featureName).size()
     distributions = []
-    for featureValue in featureValues:
-        distribution = dataset[dataset.loc[:,featureName] == featureValue].groupby(classFeatureName).size()/sizePerValue[featureValue]
+    for featureValue in feature_values:
+        distribution = dataset[dataset.loc[:,featureName] == featureValue].groupby(CLASS_COLUMN).size()/feature_value_count[feature_values]
         distributions.append(np.array(distribution,dtype=np.float32))
-    
+
     distributions = np.array(distributions, dtype=object)
-    temp = (sizePerValue/len(dataset)).to_numpy()
-    return np.dot(temp,(1 - np.array([np.sum(arr) for arr in (distributions ** 2)])))
+    feature_value_distribution = feature_value_count.to_numpy() /len(dataset)
+    return np.dot(feature_value_distribution,np.array([1 - np.sum(arr) for arr in distributions ** 2]))
 
 def pick_next_feature(dataset: pd.DataFrame)->str:
     features = dataset.columns[:-2]
-    best_feature = features[0]
-    min_gini = calculate_gini(dataset,best_feature)
-    for feature in features[1:]:
-        gini = calculate_gini(dataset,feature)
-        if gini < min_gini:
-            min_gini = gini
-            best_feature = feature
+    winnerIdx = np.array([calculate_gini(dataset,feature) for feature in features]).argmin()
+    return features[winnerIdx]
 
-    return best_feature
+def update_sample_weights(dataset: pd.DataFrame, misclassified_indices: np.ndarray, error: float) -> None:
+    classified_indices = np.delete(dataset.index.to_numpy(),misclassified_indices)
 
-def update_sample_weights(dataset: pd.DataFrame, missclassifiedSamples: np.ndarray, error: float, weightColumnName: str)->None:
-    allIndices = dataset.index
-    classifiedSamples = np.delete(allIndices.to_numpy(),missclassifiedSamples)
+    # update the weights of the successfully classified entities
+    dataset.loc[classified_indices,ENTITY_WEIGHT_COLUMN] /=  error
+    # normalize the weights of the successfully classified entities
+    dataset.loc[classified_indices,ENTITY_WEIGHT_COLUMN] /=  (np.sum(dataset.loc[classified_indices,ENTITY_WEIGHT_COLUMN]) * 2)
 
-    dataset.loc[classifiedSamples,weightColumnName] /=  error
-    dataset.loc[classifiedSamples,weightColumnName] /=  (2 * np.sum(dataset.loc[classifiedSamples,weightColumnName]))
-
-    dataset.loc[missclassifiedSamples, weightColumnName]  /= (1-error)
-    dataset.loc[missclassifiedSamples,weightColumnName] /= (np.sum(dataset.loc[missclassifiedSamples,weightColumnName] * 2))
+    # update the weights of the misclassified entities
+    dataset.loc[misclassified_indices, ENTITY_WEIGHT_COLUMN]  /= (1-error)
+    # normalize the weights of the misclassified classified entities
+    dataset.loc[misclassified_indices,ENTITY_WEIGHT_COLUMN] /= (np.sum(dataset.loc[misclassified_indices,ENTITY_WEIGHT_COLUMN]) * 2)
 
 
 def get_missclassified(dataset: pd.DataFrame, stump: TreeStump)-> np.ndarray:
-    classColumnName=dataset.columns[-2]
     missclassified = np.array([],dtype=np.int32)
     for idx in dataset.index:
         row = dataset.loc[idx,:]
         prediction = stump.classification[row[stump.featureName]]
-        if row[classColumnName] != prediction:
+        if row[CLASS_COLUMN] != prediction:
             missclassified = np.insert(missclassified,0,idx)
     return missclassified
 
+def sample_dataset(dataset: pd.DataFrame):
+    dataset_size = len(dataset)
+
+    row_indices = np.random.choice(np.arange(dataset_size),dataset_size,p=dataset[ENTITY_WEIGHT_COLUMN])
+    dataset = dataset.loc[row_indices,:]
+
+    #reset the row indices
+    dataset.reset_index(drop=True, inplace=True)
+    #normalize entity weights
+    dataset[ENTITY_WEIGHT_COLUMN] /= np.sum(dataset[ENTITY_WEIGHT_COLUMN])
+    return dataset
 
 if __name__ == '__main__':
     dataset = pd.DataFrame(
@@ -104,6 +118,8 @@ if __name__ == '__main__':
             'Hearth Disease':[True,True,True,True,False,False,False,False]
         }
     )
+
+    dataset.rename(columns={dataset.columns[-1]:CLASS_COLUMN}, inplace=True)
 
     classifier = AdaboostClassifier()
     classifier.train(dataset)
